d94eb138024fbb677a017a09e157b6f5622a663c
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-01-25 18:03:13 +0200

    Update 2024_01_25_124421_create_fandoms_table.php

--- database/migrations/2024_01_25_124421_create_fandoms_table.php
+++ database/migrations/2024_01_25_124421_create_fandoms_table.php
@@ -19,9 +19,27 @@ return new class extends Migration
                 ->collation('latin1_bin')
                 ->unique();
             $table->string('name', 255)->unique();
+
+            $table->string('fandom_category', 255)
+                ->collation('latin1_bin');
+            $table->foreign('fandom_category')
+                ->references('slug')
+                ->on('fandom_categories')
+                ->onDelete('cascade');
+
             $table->text('description')->nullable();
 
             $table->timestamps();
+        });
+
+        Schema::table('fandoms', function (Blueprint $table) {
+            $table->string('related_media_giant', 255)
+                ->collation('latin1_bin')
+                ->nullable();
+            $table->foreign('related_media_giant')
+                ->references('slug')
+                ->on('fandoms')
+                ->onDelete('set null');
         });
     }
 
