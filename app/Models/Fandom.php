c74fc11cb0281c56dab33210d5a841804acd6b74
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-01-27 00:53:14 +0200

    Методи calculatePopularity і getFandomsOrderedByFfAmount

--- app/Models/Fandom.php
+++ app/Models/Fandom.php
@@ -4,11 +4,88 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Fandom extends Model
 {
     use HasFactory;
+    use BaseGenerationTrait;
 
     protected $table = 'fandoms';
     protected $guarded = [];
+    private $db;
+    private bool $hasSlug = true;
+
+    private array $BASE_ROWS = [
+        [
+            'name' => 'Гаррі Поттер',
+            'fandom_category' => 'mediahihanty',
+            'description' => null,
+            'related_media_giant' => null,
+            'image' => 'images/fandoms/fandom-harry-potter.webp'
+        ],[
+            'name' => 'Зоряні Війни',
+            'fandom_category' => 'mediahihanty',
+            'description' => null,
+            'related_media_giant' => null,
+            'image' => 'images/fandoms/fandom-star-wars.webp'
+        ],[
+            'name' => 'Marvel',
+            'fandom_category' => 'mediahihanty',
+            'description' => null,
+            'related_media_giant' => null,
+            'image' => 'images/fandoms/fandom-marvel.webp'
+        ],[
+            'name' => 'Boku no Hero Academia',
+            'fandom_category' => 'anime-manha',
+            'description' => null,
+            'related_media_giant' => null,
+            'image' => 'images/fandoms/fandom-boku-no-hero-academia.webp'
+        ],[
+            'name' => 'Всесвіт Д.Р.Р.ТОЛКІНА',
+            'fandom_category' => 'knyhy-literatura',
+            'description' => null,
+            'related_media_giant' => null,
+            'image' => 'images/fandoms/fandom-lord-of-the-rings.webp'
+        ],
+    ];
+
+    public function __construct()
+    {
+        $this->db = DB::table($this->getTable());
+
+        if ($this->db->count() === 0)
+            // Якщо таблиця пустая, то в ній генеруються стандартні фандоми
+            $this->generate();
+    }
+
+//    public function getPopularFandoms(int $num = 5): array {
+//
+//    }
+
+    public function calculatePopularity(): void
+    {   // Рахує, скільки фанфіків в кожному фандомі
+        $fanfictions = Fanfiction::all()->groupBy('fandom'); // Збираємо усі фанфіки і групуємо за фандомами
+
+        foreach ($this->all() as $fandom) {
+            // Перевіряємо, чи існує фандом у групованому масиві
+            if (isset($fanfictions[$fandom->slug]))
+                $fandom->fictions_amount = $fanfictions[$fandom->slug]->count(); // Рахуємо, скільки фф в фандомі
+            else
+                $fandom->fictions_amount = 0; // Якщо записи фанфіків відсутні, встановлюємо 0
+
+            $fandom->save(); // Зберігаємо рядок фандому з новою кількістю
+        }
+    }
+
+    public static function getFandomsOrderedByFfAmount(int $amount)
+    {
+        $fandoms = Fandom::orderBy('fictions_amount')->take(5)->get();
+        foreach ($fandoms as $key=>$fandom) {
+            $fandom['fandom_category_name'] = FandomCategories::where('slug', $fandom['fandom_category'])->first()->name;
+            $fandoms[$key] = $fandom;
+        }
+        return $fandoms;
+    }
+
 }
