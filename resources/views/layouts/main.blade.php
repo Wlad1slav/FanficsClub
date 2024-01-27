a897ab6314ed73f553adf9aa024b469c5da2c9d0
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-01-27 17:36:16 +0200

    Створенні стилі для кнопок

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fandom-list.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
 
 </head>
 <body>
