6e8b449da633e57e0829a8dc4816550fff4dfa6d
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-01-28 16:07:40 +0200

    Update popular-fandoms-list.blade.php
    Доданий опис і додаткові параметри

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,24 @@
+<!--
+
+    Маршрут:
+    uri - '/'; назва - 'HomePage'
+
+    Контролер:
+    class:HomeController->index()
+
+    Передаються змінні:
+    string $title; string $metaDescription
+    Laravel колекція $fandoms - 5 найпопулярніших фандомів, фандоми в яких є найбільше фанфіків
+
+    Головна сторінка сайту.
+
+    Складається з:
+
+    Віджету popular-fandoms-list, що виводить контейнери з інформацією про певні фандоми.
+    Будуть виведені фандоми з колекції Laravel $fandoms
+
+-->
+
 @extends('layouts.main')
 
 @section('title')
@@ -10,7 +31,9 @@
 
 @section('content')
 
+    <h1>Фанфіки Українською Мовою</h1>
+
     <!-- Строка з найпопулярнішими ФАНДОМАМИ -->
-    @include('widgets.popular-fandoms-list')
+    @include('widgets.popular-fandoms-list', ['hasCta' => true, ''])
 
 @endsection
--- resources/views/widgets/popular-fandoms-list.blade.php
+++ resources/views/widgets/popular-fandoms-list.blade.php
@@ -1,23 +1,48 @@
+<!--
+
+    Віджет з клікабельними контейнерами, що містять інформацію про фандоми.
+    Laravel колекція $fandoms містатить фандоми, які будуть виведені.
+
+    З нативними php масивами не працює.
+
+    Приймає параметри:
+    bool $hasCta - чи буде показана кнопка з посиланням на сторінку
+    з усіма категоріями фандомів (FandomsCategoriesPage)
+    bool $hasTitle - чи буде показан h2 заголовок над фандомами
+
+-->
+
 <div id="popular-fandoms-list">
-    <h2>Найпопулярніші Фандоми</h2>
+    @if($hasTitle ?? true) <h2>
+        Найпопулярніші Фандоми</h2>
+    @endif
     <div class="list">
 
+        <!-- Цикл, що проходиться по заданому масиву фандомів -->
         @foreach($fandoms as $fandom)
-            <div class="fandom no-select clickable enlargement"
-                style="background-image: url('{{ asset($fandom->image) }}')">
-                <div class="info">
-                    <div>
-                        <p class="category">{{ $fandom->fandom_category_name }}</p>
-                        <p class="name">{{ $fandom->name }}</p>
-                    </div>
-                </div>
-            </div>
+            <a href="{{ route('CertainFandomPage', ['slug' => $fandom->slug]) }}"
+               class="fandom no-select clickable enlargement"
+               style="background-image: url('{{ asset($fandom->image) }}')">
+
+               <div class="info">
+                   <div>
+                       <p class="category">{{ $fandom->fandom_category_name }}</p>
+                       <p class="name">{{ $fandom->name }}</p>
+                   </div>
+               </div>
+
+            </a>
         @endforeach
 
     </div>
 
-
-
-
+    @if($hasCta ?? false)
+        @include('widgets.button', [
+            'title' => 'Усі фандоми',
+            'url' => route('FandomsCategoriesPage'),
+            'styles' => 'center',
+            'hoverEffect' => true
+        ])
+    @endif
 
 </div>
