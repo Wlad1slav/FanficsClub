f8bd3a5cd0a2fd8eb704d18ad77aec8ab59aae8a
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-02-03 15:17:35 +0200

    Виправлена помилка в пагінації і прибрані стрілки для руху по сторінкам

--- resources/views/widgets/pagination.blade.php
+++ resources/views/widgets/pagination.blade.php
@@ -1,17 +1,27 @@
 @if ($paginator->hasPages())
 
+    @php
+
+    $queryString = $_SERVER['QUERY_STRING'];
+
+    // Роздіє рядок запиту на масив параметрів
+    $params = explode('&', $queryString);
+
+    // Фільтрує параметри, видаляючи ті, що містять слово "page"
+    $filteredParams = array_filter($params, function($param) {
+        return strpos($param, 'page') === false;
+    });
+
+    // Збірає фільтровані параметри назад у рядок запиту
+    $filteredQueryString = implode('&', $filteredParams);
+
+    @endphp
+
     <link rel="stylesheet" href="{{ asset('css/pagination.css') }}">
 
     <p class="ff-amount">{{ $paginator->total() }} фанфіків</p>
 
     <div class="pagination">
-
-        {{-- Попередня сторінка --}}
-        @if ($paginator->onFirstPage())
-            <span class="disable row">&laquo;</span>
-        @else
-            <a class="row row" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a>
-        @endif
 
         {{-- Номери сторінок --}}
         @foreach ($elements as $element)
@@ -27,18 +37,11 @@
                         <p class="active"><span>{{ $page }}</span></p>
                     @else
                         <!-- $_SERVER['QUERY_STRING'] - запит того ж фільтру -->
-                        <a href="{{ "$url&{$_SERVER['QUERY_STRING']}" }}">{{ $page }}</a>
+                        <a href="{{ "$url&$filteredQueryString" }}">{{ $page }}</a>
                     @endif
                 @endforeach
             @endif
         @endforeach
-
-        {{-- Наступна сторінка --}}
-        @if ($paginator->hasMorePages())
-            <a class="row" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a>
-        @else
-            <span class="disabled row">&raquo;</span>
-        @endif
 
     </div>
 
