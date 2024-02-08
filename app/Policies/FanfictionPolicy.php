a2f638f65888399ce9fe50363862196e4e5e17e2
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-02-08 23:08:13 +0200

    Policy для перевірки, чи належить користувачу фанфік

---
+++ app/Policies/FanfictionPolicy.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Fanfiction;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class FanfictionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Fanfiction  $fanfiction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Fanfiction $fanfiction)
+    {
+
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Fanfiction  $fanfiction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Fanfiction $fanfiction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Fanfiction  $fanfiction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Fanfiction $fanfiction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Fanfiction  $fanfiction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Fanfiction $fanfiction)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Fanfiction  $fanfiction
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Fanfiction $fanfiction)
+    {
+        //
+    }
+
+    public function fanficAccess(User $user, Fanfiction $fanfiction)
+    {
+        return $user->id === $fanfiction->author_id;
+    }
+}
