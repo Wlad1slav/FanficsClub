e0c45cd94bdbe748031168eeb344cd946e1b8102
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-02-09 22:08:59 +0200

    Метод для зв'язку з фанфіком, якому належить розділ

--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Cache;
 
@@ -15,6 +16,11 @@ class Chapter extends Model
     protected $table = 'chapters';
     protected $guarded = [];
 
+    public function fanfiction(): BelongsTo
+    {   // Зв'язок з моделю Fanfiction
+        return $this->belongsTo(Fanfiction::class);
+    }
+
     public static function getCached(Fanfiction $fanfic)
     {
         return Cache::remember("chapters_ff_{$fanfic->id}", 60*60, function () use ($fanfic) {
@@ -24,7 +30,8 @@ class Chapter extends Model
                 // Розділи сортируються згідно з заданим порядком в бд
                 $sequence = json_decode($fanfic->chapters_sequence, true);
                 $sequenceStr = implode(',', $sequence);
-                return Chapter::whereIn('id', $sequence)
+                return Chapter::where('fanfiction_id', $fanfic->id)
+                    ->whereIn('id', $sequence)
                     ->orderByRaw("FIELD(id, {$sequenceStr})")
                     ->get();
             } else {
