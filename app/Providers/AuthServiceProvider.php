56af5e71e46dcacb522c78c0a176e64696b47776
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-02-09 22:10:17 +0200

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        'App\Models\Article' => 'App\Policies\FanfictionPolicy',
+        'App\Models\Fanfiction' => 'App\Policies\FanfictionPolicy',
     ];
 
     /**
