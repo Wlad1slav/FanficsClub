313357a5d46955d6eda515b7b3a74f7afa3b00ed
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-02-09 22:25:07 +0200

    Відверстана сторінка редагування розділу

---
+++ resources/views/fanfic-edit/chapter-edit.blade.php
@@ -0,0 +1,65 @@
+@extends('fanfic-edit.layout')
+
+<!--
+
+Параметри:
+    - chapter_title - назва розділу
+    - chapter_content - контент розділу
+    - notify - попередження перед розділом
+    - notes - нотатки, після глави
+    - is_draft - чи зберегти розділ, як чорнетку
+
+-->
+
+
+@section('content')
+
+    <link rel="stylesheet" href="{{ asset('css/fanfic/create.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/chapter/create.css') }}">
+
+    <h1>Редагувати розділ</h1>
+
+    <form method="post">
+        @csrf
+
+        <label for="chapter_title" class="required">
+            Заголовок розділу
+        </label>
+        <input type="text" name="chapter_title" id="chapter_title" required value="{{ $chapter->title }}">
+
+        <label for="chapter_content" class="required">
+            Контент розділу
+        </label>
+        <textarea name="chapter_content" id="chapter_content" required>{{ $chapter->content }}</textarea>
+
+        @php
+
+        $notes = json_decode($chapter->additional_descriptions, true);
+
+        @endphp
+
+        <label for="notify">
+            Попередження перед розділом
+        </label>
+        <textarea name="notify" id="notify">{{ $notes['notify'] }}</textarea>
+
+        <label for="notes">
+            Нотатки після розділу
+        </label>
+        <textarea name="notes" id="notes">{{ $notes['notes'] }}</textarea>
+
+        <label for="is_draft">
+            <input type="checkbox" name="is_draft" value="1" id="is_draft" @checked($chapter->is_draft)>
+            Чертнетка
+        </label>
+
+        <input type="submit" value="Зберегти">
+
+    </form>
+
+    @include('widgets.button', [
+            'title' => 'Переглянути розділ',
+            'url' => route('FanficPage', ['ff_slug' => $fanfic->slug, 'chapter_slug' => $chapter->slug]),
+        ])
+
+@endsection
