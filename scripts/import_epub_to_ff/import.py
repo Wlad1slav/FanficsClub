e7b9cd6c479569dd6a6d006b37bf7a674736a193
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-02-17 14:49:28 +0200

    Рандомна генерація id для розділів

--- scripts/import_epub_to_ff/import.py
+++ scripts/import_epub_to_ff/import.py
@@ -4,11 +4,15 @@ from slug_generate import slug_generate
 # Читання розділів фанфіку і перетворення їх на словник
 from create_dic_of_chapters import read_epub_content_to_dict
 from datetime import datetime
+import time, random
 
 # Шлях до EPUB файлу
 fanfic_original_path = 'to_import_ffs/hpimr1.epub'
 fanfic_id = 1
 is_draft = 0
+
+# Використання поточного часу як сід для генератора випадкових чисел
+random.seed(int(time.time()))
 
 # Контент фанфіку
 content_dict = read_epub_content_to_dict(fanfic_original_path)
@@ -17,10 +21,9 @@ chapters_table = rep.Repository('chapters')
 
 for title, content in content_dict.items():
     # print(f"Chapter: {title}\nContent: {content}\n\n")
-    print(f"Розділ {title} ({slug_generate(title)}) згенерований")
-
     chapters_table.create_row(
         {
+            'id': random.randint(10000, 99999),
             'fanfiction_id': fanfic_id,
             'title': str(title),
             'slug': slug_generate(title),
@@ -31,3 +34,4 @@ for title, content in content_dict.items():
         }
     )
 
+    print(f"Розділ {title} ({slug_generate(title)}) згенерований\n")
